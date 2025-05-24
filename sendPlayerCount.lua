3d5c98ed5cff69655bd92aa94a3492b32a680173
About Develouper <127244634+AboutDevelouper@users.noreply.github.com>
2025-05-24 20:02:59 -0300

    Add files via upload

---
+++ sendPlayerCount.lua
@@ -0,0 +1,25 @@
+local url = "https://bmrmta.squareweb.app/api/app"
+
+function enviarDadosServidor()
+    local tbl = toJSON({
+        serverName = getServerName(),
+        players = getPlayerCount(),
+        ip = getServerConfigSetting("serverip"),
+        status = "Online"
+    })
+
+    local options = {
+        method = "POST",
+        headers = {
+            ["Content-Type"] = "application/json"
+        },
+        postData = string.sub(tbl, 2, -2),
+    }
+    fetchRemote(url, options, callBack)
+end
+        
+function callBack(response, err)
+end
+
+setTimer(enviarDadosServidor, 60000, 0)
+enviarDadosServidor()
